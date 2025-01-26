ce433c1db58830c501af2956e16d4b0b125b3e9f
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2025-01-26 23:10:33 +0530

    Update AppApplication.java

--- backend/app/src/main/java/com/docker/app/AppApplication.java
+++ backend/app/src/main/java/com/docker/app/AppApplication.java
@@ -2,6 +2,9 @@ package com.docker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AppApplication {
@@ -10,4 +13,13 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+	@GetMapping()
+    public String index() {
+		return "forward:/index.html";
+    }
 }
